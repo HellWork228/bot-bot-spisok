7762df5b04f7b4e800c2c2522b23f54db2e28f17
Markin <64428487+HellWork228@users.noreply.github.com>
2023-05-31 19:29:16 +0300

    Create discoord.py

---
+++ discoord.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from fucti import fuct_in
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(name='bot')
+async def _bot(ctx):
+    await ctx.send('Yes, the bot is cool.')
+
+@bot.command()
+async def факт(ctx):
+    await ctx.send(fuct_in())
+
+bot.run("токен")
